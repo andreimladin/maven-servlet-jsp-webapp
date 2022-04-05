24144ec9ce887d9b8986ebbb35ec02219a0562ac
andrei <andrei.mladin@gmail.com>
2022-04-05 15:31:39 +0300

    first commit

---
+++ src/main/java/ro/iteahome/java/advanced/servlet/HelloServlet.java
@@ -0,0 +1,42 @@
+package ro.iteahome.java.advanced.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public final class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws IOException, ServletException {
+
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<title>Sample Application Servlet Page</title>");
+        writer.println("</head>");
+        writer.println("<body bgcolor=white>");
+
+        writer.println("<table border=\"0\" cellpadding=\"10\">");
+        writer.println("<tr>");
+        writer.println("<td>");
+        writer.println("<img src=\"images/Pivotal_Logo.png\">");
+        writer.println("</td>");
+        writer.println("<td>");
+        writer.println("<h1>Sample Application Servlet</h1>");
+        writer.println("</td>");
+        writer.println("</tr>");
+        writer.println("</table>");
+
+        writer.println("This is the output of a servlet that is part of");
+        writer.println("the Hello, World application.");
+
+        writer.println("</body>");
+        writer.println("</html>");
+        writer.flush();
+    }
+}
